c53f88b85bba3b18a3724d6c654da812ece3cf7d
blessdarah <blessdarahuba@gmail.com>
2022-02-07 00:05:20 +0100

    update subscriptions controller with trycatch
    Add subcribe and unsubscribe functionality in try/catch

--- app/Http/Controllers/SubscriptionsController.php
+++ app/Http/Controllers/SubscriptionsController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Resources\PackageResource;
 use App\Models\Package;
 use App\Models\User;
+use PhpParser\Node\Stmt\TryCatch;
+use Throwable;
 
 class SubscriptionsController extends Controller
 {
@@ -19,25 +21,41 @@ class SubscriptionsController extends Controller
 
     public function subscribe($id)
     {
-        $package = Package::find($id);
-        auth()->user()->subscriptions()->attach($package->id);
-        return response()->json([
-            "success" => true,
-            "message" => "You have subscribe to this package",
-            "data" =>  new PackageResource($package)
-        ]);
+        try {
+            $package = Package::find($id);
+            auth()->user()->subscriptions()->attach($package->id);
+            return response()->json([
+                "success" => true,
+                "message" => "You have subscribe to this package",
+                "data" =>  new PackageResource($package)
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                "success" => false,
+                "message" => $th->getMessage(),
+                "data" =>  null
+            ]);
+        }
     }
 
 
     public function unsubscribe($id)
     {
-        $package = Package::find($id);
-        auth()->user()->subscriptions()->detach($package);
+        try {
+            $package = Package::find($id);
+            auth()->user()->subscriptions()->detach($package);
 
-        return response()->json([
-            "success" => true,
-            "message" => "You have unscribed to this package",
-            "data" => []
-        ]);
+            return response()->json([
+                "success" => true,
+                "message" => "You have unscribed to this package",
+                "data" => []
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                "success" => false,
+                "message" => $th->getMessage(),
+                "data" =>  null
+            ]);
+        }
     }
 }
