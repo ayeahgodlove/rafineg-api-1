bf262f9cb6e73284b0da6c1fce23a03a942cafaf
blessdarah <blessdarahuba@gmail.com>
2022-02-11 20:36:09 +0100

    Wrap payment in trycatch block
    Catch payment errors in registration controller

--- app/Http/Controllers/RegistrationFeesController.php
+++ app/Http/Controllers/RegistrationFeesController.php
@@ -11,140 +11,144 @@ use Malico\MeSomb\Payment;
 
 class RegistrationFeesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return response()->json([
-            "success" => true,
-            "data" => RegistrationFeeResource::collection(RegistrationFee::all()),
-        ]);
-    }
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
+		return response()->json([
+			"success" => true,
+			"data" => RegistrationFeeResource::collection(RegistrationFee::all()),
+		]);
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\CreateRegistrationFeeRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(CreateRegistrationFeeRequest $request)
-    {
-        $data = $request->validated();
-        //registration fee transaction
-        $data['username'] = auth()->user()->name;
-        $data['user_id'] = auth()->user()->id;
-        $data['amount'] = "30";
-        $tel = $data['phone_number'];
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\CreateRegistrationFeeRequest  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(CreateRegistrationFeeRequest $request)
+	{
+		try {
 
-        $transaction = new Payment("$tel", $data['amount']);
+			$data = $request->validated();
+			//registration fee transaction
+			$data['username'] = auth()->user()->name;
+			$data['user_id'] = auth()->user()->id;
+			$data['amount'] = "30";
+			$tel = $data['phone_number'];
 
-        $deposit = $transaction->pay();
-        $currentUser = User::find(auth()->user()->id);
+			$transaction = new Payment("$tel", $data['amount']);
 
-        $registrationFee = null;
-        if ($deposit->success) {
-            // Fire some event, send payout email
-            $registrationFee = RegistrationFee::create($data);
-            $currentUser['is_registered'] = true;
-            $currentUser->save();
+			$deposit = $transaction->pay();
+			$currentUser = User::find(auth()->user()->id);
 
-            // update user cashbox
+			$registrationFee = null;
+			if ($deposit->success) {
+				// Fire some event, send payout email
+				$registrationFee = RegistrationFee::create($data);
+				$currentUser['is_registered'] = true;
+				$currentUser->save();
+
+				// update user cashbox
 
 
-            return response()->json([
-                "data" => [
-                    "registrationFee" => new RegistrationFeeResource($registrationFee),
-                    "user" => new UserResource($currentUser),
-                ],
-                "success" => true
-            ]);
-        }
+				return response()->json([
+					"data" => [
+						"registrationFee" => new RegistrationFeeResource($registrationFee),
+						"user" => new UserResource($currentUser),
+					],
+					"success" => true
+				]);
+			}
 
-        return response()->json([
-            "success" => false,
-            "message" => "payment failed",
-            "data" => [],
-        ]);
-    }
+		} catch (\Throwable $th) {
+			return response()->json([
+				"success" => false,
+				"message" => "Payment failed" . $th->getMessage(),
+				"data" => null
+			]);
+		}
+	}
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show(RegistrationFee $registrationFee)
-    {
-        if ($registrationFee->exits()) {
-            return response()->json([
-                "success" => true,
-                "data" => new RegistrationFeeResource($registrationFee),
-                "message" => ""
-            ]);
-        }
-        return response()->json([
-            "success" => false,
-            "data" => null,
-            "message" => "RegistrationFee does not exist."
-        ]);
-    }
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show(RegistrationFee $registrationFee)
+	{
+		if ($registrationFee->exits()) {
+			return response()->json([
+				"success" => true,
+				"data" => new RegistrationFeeResource($registrationFee),
+				"message" => ""
+			]);
+		}
+		return response()->json([
+			"success" => false,
+			"data" => null,
+			"message" => "RegistrationFee does not exist."
+		]);
+	}
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(CreateRegistrationFeeRequest $request, $id)
-    {
-        $registrationFee = RegistrationFee::find($id);
-        if ($registrationFee->exits()) {
-            $data = $request->validated();
-            $registrationFee->update($data);
-            return response()->json([
-                "success" => true,
-                "data" => new RegistrationFeeResource($registrationFee),
-                "message" => "Registration fee has been updated successfully"
-            ]);
-        }
-        return response()->json([
-            "success" => false,
-            "data" => null,
-            "message" => "RegistrationFee could not be updated"
-        ]);
-    }
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(CreateRegistrationFeeRequest $request, $id)
+	{
+		$registrationFee = RegistrationFee::find($id);
+		if ($registrationFee->exits()) {
+			$data = $request->validated();
+			$registrationFee->update($data);
+			return response()->json([
+				"success" => true,
+				"data" => new RegistrationFeeResource($registrationFee),
+				"message" => "Registration fee has been updated successfully"
+			]);
+		}
+		return response()->json([
+			"success" => false,
+			"data" => null,
+			"message" => "RegistrationFee could not be updated"
+		]);
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        $registrationFee = RegistrationFee::find($id);
-        if ($registrationFee->exits()) {
-            if ($registrationFee->delete()) {
-                return response()->json([
-                    "success" => true,
-                    "data" => null,
-                    "message" => "RegistrationFee deleted successfully"
-                ], 201);
-            }
-            return response()->json([
-                "success" => false,
-                "data" => null,
-                "message" => "RegistrationFee could not be deleted"
-            ]);
-        }
-        return response()->json([
-            "success" => false,
-            "data" => null,
-            "message" => "RegistrationFee does not exist"
-        ]);
-    }
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy($id)
+	{
+		$registrationFee = RegistrationFee::find($id);
+		if ($registrationFee->exits()) {
+			if ($registrationFee->delete()) {
+				return response()->json([
+					"success" => true,
+					"data" => null,
+					"message" => "RegistrationFee deleted successfully"
+				], 201);
+			}
+			return response()->json([
+				"success" => false,
+				"data" => null,
+				"message" => "RegistrationFee could not be deleted"
+			]);
+		}
+		return response()->json([
+			"success" => false,
+			"data" => null,
+			"message" => "RegistrationFee does not exist"
+		]);
+	}
 }
--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,6 @@ use App\Models\User;
 use App\Http\Controllers\RegistrationFeesController;
 use App\Http\Controllers\SavingsController;
 use App\Http\Controllers\SubscriptionsController;
-use App\Http\Controllers\TransactionsController;
-use Laravel\Cashier\Http\Controllers\PaymentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,28 +32,28 @@ Route::post('verify/${method}', [AuthController::class, 'verify_client']);
 Route::get('verification-code/${method}', [AuthController::class, 'send_verification_code']);
 
 Route::get('test', fn () => response()->json([
-    "success" => true,
-    "data" => UserResource::collection(User::all()),
+	"success" => true,
+	"data" => UserResource::collection(User::all()),
 ]));
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
-    Route::apiResource('users', UsersController::class);
-    Route::apiResource('packages', PackagesController::class);
-    Route::apiResource('profiles', ProfilesController::class);
-    Route::apiResource('contracts', ContractsController::class);
-    Route::apiResource('referals', ReferalController::class);
-    Route::apiResource('registration', RegistrationFeesController::class);
-    Route::apiResource('savings', SavingsController::class);
-    Route::apiResource('njangiGroups', NjangiGroupController::class)->except(['create', 'show', 'edit']);
-    Route::post('subscribe/{package_id}', [SubscriptionsController::class, 'subscribe']);
-    Route::post('unsubscribe/{package_id}', [SubscriptionsController::class, 'unsubscribe']);
-    Route::get('subscriptions', [SubscriptionsController::class, 'index']);
-    // Route::post('payments/sendMoney', [PaymentController::class, 'sendMoney']);
+	Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+	Route::apiResource('users', UsersController::class);
+	Route::apiResource('packages', PackagesController::class);
+	Route::apiResource('profiles', ProfilesController::class);
+	Route::apiResource('contracts', ContractsController::class);
+	Route::apiResource('referals', ReferalController::class);
+	Route::apiResource('registration', RegistrationFeesController::class);
+	Route::apiResource('savings', SavingsController::class);
+	Route::apiResource('njangiGroups', NjangiGroupController::class)->except(['create', 'show', 'edit']);
+	Route::post('subscribe/{package_id}', [SubscriptionsController::class, 'subscribe']);
+	Route::post('unsubscribe/{package_id}', [SubscriptionsController::class, 'unsubscribe']);
+	Route::get('subscriptions', [SubscriptionsController::class, 'index']);
+	// Route::post('payments/sendMoney', [PaymentController::class, 'sendMoney']);
 });
 Route::fallback(function () {
-    return response()->json([
-        "success" => false,
-        "message" => 'Page Not Found. If error persists, contact info@website.com'
-    ], 404);
+	return response()->json([
+		"success" => false,
+		"message" => 'Page Not Found. If error persists, contact info@website.com'
+	], 404);
 });
