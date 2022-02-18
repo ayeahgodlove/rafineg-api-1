0f633233887156a3ab26ef4fbb7ebd4412a02ee9
blessdarah <blessdarahuba@gmail.com>
2022-02-18 03:08:56 +0100

    update profiles controller

--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,6 @@ use App\Models\User;
 use App\Http\Controllers\RegistrationFeesController;
 use App\Http\Controllers\SavingsController;
 use App\Http\Controllers\SubscriptionsController;
-use App\Http\Controllers\TransactionsController;
-use Laravel\Cashier\Http\Controllers\PaymentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -35,31 +33,31 @@ Route::post('forgot-password', [AuthController::class, 'forgot_password']);
 Route::post('forgot-password/confirm-code', [AuthController::class, 'confirm_password_reset_code']);
 
 Route::get('test', fn () => response()->json([
-    "success" => true,
-    "data" => UserResource::collection(User::all()),
+	"success" => true,
+	"data" => UserResource::collection(User::all()),
 ]));
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
-    Route::apiResource('users', UsersController::class);
-    Route::apiResource('packages', PackagesController::class);
-    Route::apiResource('profiles', ProfilesController::class);
-    Route::apiResource('contracts', ContractsController::class);
-    Route::apiResource('referals', ReferalController::class);
-    Route::apiResource('registration', RegistrationFeesController::class);
-    Route::apiResource('savings', SavingsController::class);
-    Route::apiResource('njangi-groups', NjangiGroupController::class)->except(['create', 'show', 'edit']);
-    Route::apiResource('cash-transfers', NjangiGroupController::class)->except(['create', 'edit']);
-    Route::post('subscribe/{package_id}', [SubscriptionsController::class, 'subscribe']);
-    Route::post('unsubscribe/{package_id}', [SubscriptionsController::class, 'unsubscribe']);
-    Route::get('subscribers/{package_id}', [SubscriptionsController::class, 'subscribers']);
-    Route::get('subscriptions', [SubscriptionsController::class, 'index']);
-    // Route::get('subscribers', [SubscriptionsController::class, 'index']);
-    // Route::post('payments/sendMoney', [PaymentController::class, 'sendMoney']);
+	Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+	Route::apiResource('users', UsersController::class);
+	Route::apiResource('packages', PackagesController::class);
+	Route::apiResource('profiles', ProfilesController::class);
+	Route::apiResource('contracts', ContractsController::class);
+	Route::apiResource('referals', ReferalController::class);
+	Route::apiResource('registration', RegistrationFeesController::class);
+	Route::apiResource('savings', SavingsController::class);
+	Route::apiResource('njangi-groups', NjangiGroupController::class)->except(['create', 'show', 'edit']);
+	Route::apiResource('cash-transfers', NjangiGroupController::class)->except(['create', 'edit']);
+	Route::post('subscribe/{package_id}', [SubscriptionsController::class, 'subscribe']);
+	Route::post('unsubscribe/{package_id}', [SubscriptionsController::class, 'unsubscribe']);
+	Route::get('subscribers/{package_id}', [SubscriptionsController::class, 'subscribers']);
+	Route::get('subscriptions', [SubscriptionsController::class, 'index']);
+	// Route::get('subscribers', [SubscriptionsController::class, 'index']);
+	// Route::post('payments/sendMoney', [PaymentController::class, 'sendMoney']);
 });
 Route::fallback(function () {
-    return response()->json([
-        "success" => false,
-        "message" => 'Page Not Found. If error persists, contact info@website.com'
-    ], 404);
+	return response()->json([
+		"success" => false,
+		"message" => 'Page Not Found. If error persists, contact info@website.com'
+	], 404);
 });
